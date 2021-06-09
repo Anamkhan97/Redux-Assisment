6772168473d0af717dcc437046ca49308c9ca3f5
Anamkhan97 <84963467+Anamkhan97@users.noreply.github.com>
2021-06-09 12:13:23 +0530

    Update mockContent.js

--- src/mock/mockContent.js
+++ src/mock/mockContent.js
@@ -1,5 +1,21 @@
-const content ={
+const content = {
     heading: "Region-Country Selector",
-    regions :['Africa', 'Asia','Europe', 'Americas','Oceania']
+    regions: [
+        {
+            id: 'Africa',
+            name: 'Africa'
+        },
+        {
+            id: 'Europe',
+            name: 'Europe'
+        },
+        {
+            id: 'Americas',
+            name: 'Americas'
+        },
+        {
+            id: 'Oceania',
+            name: 'Oceania'
+        }]
 }
 export default content;
