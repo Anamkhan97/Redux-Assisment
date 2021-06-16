fc65c455fa2ad353cd9bf62904a5345ead7e9ecd
Anamkhan97 <84963467+Anamkhan97@users.noreply.github.com>
2021-06-16 09:42:24 +0530

    Update countrySelector.js

--- src/components/countrySelector.js
+++ src/components/countrySelector.js
@@ -14,7 +14,7 @@ class CountrySelector extends React.Component {
             selectedCountry:''
         };
        
-        this.changeCountry = this.changeCountry.bind(this);
+        
     }
     componentDidMount() {
         this.setCountries(this.props.Content.regions[0].name );
@@ -30,11 +30,7 @@ class CountrySelector extends React.Component {
         return null;
     }
 
-    changeCountry(event) {
-        this.setState({ selectedRegion: event.target.value });
-        this.props.getCountries(event.target.value);
-    }
-
+    
     setCountries(region) {
         this.props.getCountries(region);
         if (this.props.countries) {
@@ -51,21 +47,11 @@ class CountrySelector extends React.Component {
                 <h1>{this.props.Content.heading}</h1>
                 <div className="dropdown">
                 <label>Select Region </label>
-               {/*  <select value={this.state.selectedRegion} onChange={this.changeCountry}>
-                    {this.props.Content.regions.map((region, i) => {
-                        return <option key={i}>{region}</option>
-                    })}
-                </select>   */}        
-                <SelectComponent value={this.state.selectedRegion} onChange={this.changeCountry} dropdown={this.props.Content.regions}/>     
+                  
+                <SelectComponent value={this.state.selectedRegion} handleDropdownChange={(region) =>this.setCountries(region)} dropdown={this.props.Content.regions}/>     
                 <br />              
                 <label>Select Country </label>
-               {/*  <select >
-                    {this.props.countries.map(
-                        (country) => {
-                            return <option >{country.name}</option>;
-                        }
-                    )}
-                </select> */}
+              
                 <SelectComponent dropdown={this.props.countries}/>
                 </div>
             </div>
